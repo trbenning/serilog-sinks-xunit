4110c708f4ec65d9d37f4d7fde397e00a7f5a706
Todd Benning <todd@benning.io>
2017-05-18 00:54:30 -0500

    Create a basic test

---
+++ test/Serilog.Sinks.XUnit.Tests/IntegratoinTests.cs
@@ -0,0 +1,34 @@
+namespace Serilog.Sinks.XUnit.Tests
+{
+    using System;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class IntegratoinTests
+    {
+        ILogger _output;
+
+        public IntegratoinTests(ITestOutputHelper output)
+        {
+            // Pass the ITestOutputHelper object to the TestOutput sink
+            _output = new LoggerConfiguration()
+                .WriteTo.TestOutput(output, Events.LogEventLevel.Verbose)
+                .CreateLogger()
+                .ForContext<IntegratoinTests>();
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void ExampleUsage()
+        {
+            // Use ILogger as you normally would. These messages will show up in the test output
+            _output.Information("Test output to Serilog!");
+
+            Action sketchy = () => throw new Exception("I threw up.");
+            var exception = Record.Exception(sketchy);
+
+            _output.Error(exception, "Here is an error.");
+            Assert.NotNull(exception);
+        }
+    }
+}
