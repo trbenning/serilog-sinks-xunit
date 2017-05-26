742320e807f856fc368847b4b778f8925f7a9388
Todd Benning <todd.benning@wayne.com>
2017-05-25 23:22:33 -0500

    Add unit tests

---
+++ test/Serilog.Sinks.XUnit.Tests/TestOutputSinkTests.cs
@@ -0,0 +1,61 @@
+﻿namespace Serilog.Sinks.XUnit.Tests
+{
+    using FluentAssertions;
+    using NSubstitute;
+    using Serilog.Events;
+    using Serilog.Formatting;
+    using Serilog.Parsing;
+    using System;
+    using System.IO;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class TestOutputSinkTests
+    {
+        [Fact]
+        public void Constructor_ShouldThrowIfTestOutputHelperIsNull()
+        {
+            var ex = Record.Exception(() => new TestOutputSink(null, Substitute.For<ITextFormatter>()));
+            ex.Should().BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowIfTextFormatterIsNull()
+        {
+            var ex = Record.Exception(() => new TestOutputSink(Substitute.For<ITestOutputHelper>(), null));
+            ex.Should().BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Emit_ShouldThrowIfEventIsNull()
+        {
+            var underTest = new TestOutputSink(
+                Substitute.For<ITestOutputHelper>(),
+                Substitute.For<ITextFormatter>());
+
+            var ex = Record.Exception(() => underTest.Emit(null));
+
+            ex.Should().BeOfType<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Emit_ShouldWriteFormattedEventToTestOutput()
+        {
+            var outputHelper = Substitute.For<ITestOutputHelper>();
+            var formatter = Substitute.For<ITextFormatter>();
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.TestOutput(outputHelper, formatter)
+                .CreateLogger();
+
+            const string message = "Hello";
+            logger.Information(message);
+
+            outputHelper.Received(1).WriteLine(Arg.Any<string>());
+
+            formatter.Received(1).Format(
+                Arg.Is<LogEvent>(ev => ev.Level == LogEventLevel.Information && ev.MessageTemplate.Text == message),
+                Arg.Any<StringWriter>());
+        }
+    }
+}
