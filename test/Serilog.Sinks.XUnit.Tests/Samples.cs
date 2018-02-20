e57af9f24414debe60b0681bce6722b00a82d38b
Todd Benning <todd.benning@doverfs.com>
2018-02-20 12:33:35 -0600

    Add tests to verify issue #1

--- test/Serilog.Sinks.XUnit.Tests/Samples.cs
+++ test/Serilog.Sinks.XUnit.Tests/Samples.cs
@@ -6,12 +6,13 @@ namespace Serilog.Sinks.XUnit.Tests
 
     public class Samples
     {
-        readonly ILogger _output;
+        readonly ILogger _log;
 
         public Samples(ITestOutputHelper output)
         {
             // Pass the ITestOutputHelper object to the TestOutput sink
-            _output = new LoggerConfiguration()
+            _log = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
                 .WriteTo.TestOutput(output, Events.LogEventLevel.Verbose)
                 .CreateLogger()
                 .ForContext<Samples>();
@@ -22,13 +23,29 @@ namespace Serilog.Sinks.XUnit.Tests
         public void ExampleUsage()
         {
             // Use ILogger as you normally would. These messages will show up in the test output
-            _output.Information("Test output to Serilog!");
+            _log.Information("Test output to Serilog!");
 
             Action sketchy = () => throw new Exception("I threw up.");
             var exception = Record.Exception(sketchy);
 
-            _output.Error(exception, "Here is an error.");
+            _log.Error(exception, "Here is an error.");
             Assert.NotNull(exception);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void VerifyDebugLevelOutput()
+        {
+            _log.Debug("Test output to Serilog!");
+            // Check the test output window. You should see the above message.
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void VerifyVerboseLevelOutput()
+        {
+            _log.Verbose("Test output to Serilog!");
+            // Check the test output window. You should see the above message.
         }
     }
 }
